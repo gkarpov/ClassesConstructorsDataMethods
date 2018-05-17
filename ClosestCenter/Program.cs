94e92034184d1b25c15fe3d0f4b1086ef3e57d1a
Georgi Karpov <karpov21g@gmail.com>
2018-05-17 18:33:22 +0300

    Problems 10, 11

---
+++ ClosestCenter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClosestCenter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            if (Distance(x1, y1) <= Distance(x2, y2))
+            {
+                Console.WriteLine("({0}, {1})", x1, y1);
+            }else
+            {
+                Console.WriteLine("({0}, {1})", x2, y2);
+            }
+        }
+
+        static double Distance(double x, double y)
+        {
+            return (Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)));
+        }
+    }
+}
---
+++ GeomCalc/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeomCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Func = Console.ReadLine();
+            FuncChooser(Func);
+            
+        }
+
+        static void FuncChooser(string figure)
+        {
+            switch (figure)
+            {
+                case "triangle":
+                    Console.WriteLine("{0:0.00}", TriArea(double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine())));
+                    break;
+                case "square":
+                    Console.WriteLine("{0:0.00}", SquareArea(double.Parse(Console.ReadLine())));
+                    break;
+                case "rectangle":
+                    Console.WriteLine("{0:0.00}", RectangleArea(double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine())));
+                    break;
+                case "circle":
+                    Console.WriteLine("{0:0.00}", CircleArea(double.Parse(Console.ReadLine())));
+                    break;
+                default:
+                    break;
+
+            }
+        }
+
+        static double TriArea(double side, double height)
+        {
+            return side * height / 2;
+        }
+
+        static double SquareArea(double side)
+        {
+            return side * side;
+        }
+
+        static double RectangleArea(double side, double height)
+        {
+            return side * height;
+        }
+
+        static double CircleArea(double side)
+        {
+            return Math.PI* Math.Pow(side, 2);
+        }
+    }
+}
