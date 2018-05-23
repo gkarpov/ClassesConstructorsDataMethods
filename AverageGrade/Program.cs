2febbaff6fae258d709f282d6fc302790db2c289
Georgi Karpov <karpov21g@gmail.com>
2018-05-23 19:41:06 +0300

    till BookLibrary

---
+++ AverageGrade/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AverageGrade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            
+            List<Student> studentList = ReadAllStudents(num);
+
+            GradeComparer gc = new GradeComparer();
+            
+
+            studentList = FilterStudents(studentList, 5);
+            //studentList = studentList.OrderBy(obj => obj.AverageGrade).ToList();
+            studentList.Sort(gc);
+            studentList = studentList.OrderBy(obj => obj.Name).ToList();
+            
+            PrintStudents(studentList);
+        }
+        
+        static List<Student> ReadAllStudents(int num)
+        {
+            List<Student> tmp = new List<Student>();
+            for (int i = 0; i < num; i++)
+            {   
+                var stud  = new Student();
+                stud.ReadStudent(Console.ReadLine());
+                tmp.Add(stud);
+
+            }
+
+            return tmp;
+        }
+
+        static List<Student> FilterStudents(List<Student> list, double grade)
+        {
+            
+            for (int i = list.Count-1; i >=0; i--)
+            {
+                if (list.ElementAt(i).AverageGrade < grade)
+                    list.RemoveAt(i);
+            }
+
+            return list;
+        }
+
+        static void PrintStudents(List<Student> list)
+        {
+            //list.Sort();
+
+            foreach(var stud in list)
+                Console.WriteLine("{0} -> {1:0.00}", stud.Name, stud.AverageGrade);
+            
+        }
+
+
+    }
+
+    class Student
+    {
+        public string Name { get; set; }
+        public double[] Grades { get; set; }
+        public double AverageGrade
+        {
+            get {
+                return Average(Grades);
+            }
+        }
+
+        double Average(double[] grades)
+        {
+            double result = 0;
+            foreach (var gr in grades)
+            {
+                result += gr;
+
+            }
+            return result / grades.Count();
+        }
+
+        public void ReadStudent(string input)
+        {
+            String[] tmp = input.Split(' ').ToArray();
+            double[] notes = new double[tmp.Count()-1];
+
+            this.Name = tmp[0];
+            for (int i = 1; i < tmp.Count();i++ )
+                notes[i-1] = double.Parse(tmp[i]);
+            this.Grades = notes;
+        }
+    }
+
+
+
+    class GradeComparer : IComparer<Student>
+    {
+        public int Compare(Student o1, Student o2)
+        {
+            if (o1.AverageGrade < o2.AverageGrade)
+            {
+                return 1;
+            }
+            else if (o1.AverageGrade > o2.AverageGrade)
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+    }
+
+    
+}
---
+++ BookLibrary/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BookLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = int.Parse(Console.ReadLine());
+
+            Library mybooks = new Library();
+            mybooks.ReadAllBooks(num);
+
+            mybooks.AuthorPrice();
+
+        }
+    }
+
+    class Library
+    {
+        public string Name { get; set; }
+        public List<Book> Books { get; set; }
+        public List<string> Authors{get{return GetAuthors(this);}}
+        public void ReadAllBooks(int num)
+        {
+            List<Book> tmp = new List<Book>();
+            for (int i = 0; i < num; i++)
+            {
+                var book = new Book();
+                book.getBook(Console.ReadLine());
+                tmp.Add(book);
+            }
+            this.Books = tmp;
+        }
+
+        public List<string> GetAuthors(Library lib)
+        {
+            List<string> auths = new List<string>();
+            for (int i = 0; i < lib.Books.Count; i++)
+            {
+                if (!auths.Contains(lib.Books[i].author))
+                    auths.Add(lib.Books[i].author);
+            }
+            return auths;
+        }
+        
+        public Dictionary<string, double> AvgPrice()
+        {
+            double avgprice = 0;
+            int n=0;
+
+            Dictionary<string, double> AutPrices = new Dictionary<string, double>();
+            
+            foreach(var a in this.GetAuthors(this))
+            {    
+                for (int i = 0; i < this.Books.Count; i++)
+                {
+
+                    if (this.Books[i].author == a)
+                    {
+                        avgprice+=this.Books[i].price;
+                        n++;
+                    }
+                }
+                //AutPrices[a]=avgprice/n;
+                AutPrices[a]=avgprice;
+                avgprice = 0;
+                n=0;
+            }
+
+            return AutPrices;
+    }
+
+        
+    public void AuthorPrice()
+    {
+        Dictionary<string, double> dict = this.AvgPrice();
+        var items = from pair in dict
+                    orderby pair.Value descending
+                    select pair;
+
+        //foreach (var aut in dict)
+        //{
+        //    Console.WriteLine("{0} -> {1:0.00}", aut.Key, aut.Value);
+
+        //}
+        foreach (KeyValuePair<string, double> pair in items)
+        {
+            Console.WriteLine("{0} -> {1:0.00}", pair.Key, pair.Value);
+        }
+    }
+    }
+
+    class Book
+    {
+        
+        public string title { get; set; }
+        public string author { get; set; }
+        public string publisher { get; set; }
+        public DateTime rdate { get; set; }
+        public string ISBN { get; set; }
+        public double price { get; set; }
+
+        public void getBook(string input)
+        {
+            string[] inp = input.Split(' ').ToArray();
+
+            this.title = inp[0];
+            this.author = inp[1];
+            this.publisher = inp[2];
+            this.rdate = DateTime.Parse(inp[3]);
+            this.ISBN = inp[4];
+            this.price = double.Parse(inp[5]);
+        }
+
+
+    }
+}
