e00a1c32520ee102fed25eeddaeb091aae810f69
Georgi Karpov <karpov21g@gmail.com>
2018-05-28 19:16:08 +0300

    Add DateTime options

---
+++ AndeyBilliard/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AndeyBilliard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            shop Myshop = new shop();
+            int num = int.Parse(Console.ReadLine());
+
+            Myshop.addprod(num);
+
+            Costumers mycostumers = new Costumers();
+
+            mycostumers.fillcart(Myshop);
+
+            mycostumers.Bill(Myshop);
+
+
+        }
+
+        class shop
+        {
+            public Dictionary<string, decimal> products { get; set; }
+
+            public void addprod(int num)
+            {
+                string[] product = new string[2];
+                products = new Dictionary<string, decimal>();
+
+                for (int i = 0; i < num; i++)
+                {
+                    product = Console.ReadLine().Split('-').ToArray();
+                    //if(products.ContainsKey(product[0]))
+
+                    products[product[0]] = decimal.Parse(product[1]);
+
+                }
+            }
+
+        }
+
+        class Costumers
+        {
+
+            SortedDictionary<string, Dictionary<string, int>> shoplist = new SortedDictionary<string, Dictionary<string, int>>();
+
+            public void fillcart(shop myshop)
+            {
+                string[] buy = new string[2];
+                string[] tmp = new string[2];
+
+                while (true)
+                {
+                    buy = Console.ReadLine().Split('-').ToArray();
+                    if (buy[0] == "end of clients")
+                        break;
+
+                    tmp = buy[1].Split(',').ToArray();
+                    if (myshop.products.ContainsKey(tmp[0]))
+                    {
+                        Dictionary<string, int> tmpdict = new Dictionary<string, int>();
+                        tmpdict[tmp[0]] = int.Parse(tmp[1]); 
+                        if (shoplist.ContainsKey(buy[0]))
+                        {
+                            if(shoplist[buy[0]].ContainsKey(tmp[0]))
+                                shoplist[buy[0]][tmp[0]] += int.Parse(tmp[1]); 
+                            else
+                                shoplist[buy[0]].Add(tmp[0], int.Parse(tmp[1]));
+                        }
+                        else
+                           shoplist[buy[0]] = tmpdict;
+                    }
+                }
+            }
+
+            public void Bill(shop myshop)
+            {
+                decimal totbil = 0;
+
+                foreach (var nam in this.shoplist.Keys)
+                {
+                    Console.WriteLine("{0}", nam);
+                    decimal tmpbil = 0;
+
+                    foreach (var prod in this.shoplist[nam])
+                    {
+                        
+                        
+                        Console.WriteLine("-- {0} - {1}", prod.Key, prod.Value);
+                        tmpbil += prod.Value * myshop.products[prod.Key];
+                        totbil += prod.Value * myshop.products[prod.Key];
+                        
+                    }
+                    Console.WriteLine("Bill: {0:F2}", tmpbil);
+                }
+                Console.WriteLine("Total bill: {0:F2}", totbil);
+            }
+        }
+
+    }
+}
---
+++ MentorGroup/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MentorGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, Student> students = new SortedDictionary<string, Student>();
+            students = ReadStudents();
+
+            GroupOutput(students);
+
+        }
+
+        static void GroupOutput(SortedDictionary<string, Student> students)
+        {
+            foreach (var st in students)
+            {
+                Console.WriteLine("{0}", st.Key);
+                Console.WriteLine("Comments:");
+                foreach(var com in st.Value.Comments)
+                    Console.WriteLine("- {0}", com);
+                Console.WriteLine("Dates attended:");
+                foreach(var dat in st.Value.Dates)
+                    Console.WriteLine("-- {0}", dat);
+            }
+
+        }
+
+
+        static SortedDictionary<string, Student> ReadStudents()
+        {
+            SortedDictionary<string, Student> studs = new SortedDictionary<string, Student>();
+            
+
+            while (true)
+            {
+                Student tmp = new Student();
+                string inp =  Console.ReadLine();
+                if (inp != "end of dates")
+                {
+                var input =inp.Split(new char[] { ' ', ',' }).ToArray();
+                
+                    //tmp.name = input[0];
+                    for (int i = 1; i < input.Count(); i++)
+                        tmp.Dates.Add(DateTime.Parse(input[i]));
+
+                    studs[input[0]] = tmp;
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+            
+            while (true)
+            {
+                Student tmp = new Student();
+                var input = Console.ReadLine().Split('-').ToArray();
+                if (input[0] != "end of comments")
+                {
+                    //tmp.name = input[0];
+                    if (studs.ContainsKey(input[0]))
+                    {
+                        for (int i = 1; i < input.Count(); i++)
+                            tmp.Comments.Add(input[i]);
+
+                        studs[input[0]].Comments = tmp.Comments;
+                    }   
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+            return studs;
+            
+        }
+
+
+    }
+
+    class Student
+    {
+        public List<string> Comments { get; set; }
+        public List<DateTime> Dates { get; set; }
+
+        public Student()
+        {
+            Comments = new List<string>();
+            Dates = new List<DateTime>();
+        }
+
+    }                   
+}
